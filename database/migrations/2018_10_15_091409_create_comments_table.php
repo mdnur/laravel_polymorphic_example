354bef2513d9c73318ef79a3c126effcfa5ce6ee
Md Nur <mdnur701@gmail.com>
2018-10-15 16:01:35 +0600

    user_id field add in Commments Migration

--- database/migrations/2018_10_15_091409_create_comments_table.php
+++ database/migrations/2018_10_15_091409_create_comments_table.php
@@ -15,6 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id');
             $table->text('body');
             $table->integer('commentable_id');
             $table->string('commentable_type');
